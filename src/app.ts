b934e1347f0362813efec55ffe5be51654c416f4
Linder Hassinger <linderhassinger00@gmail.com>
2023-02-02 10:44:08 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { type Application } from "express";
 import * as ROUTER from "./components";
 import connect from "./datasource";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(cors());
 
 connect().catch((error) => console.log(`Error database mongo: ${error}`));
 
